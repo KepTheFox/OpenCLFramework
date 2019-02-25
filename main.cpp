9305fbd09a7c36a3283ae00b3561484284200604
KepTheFox <kepthefox@gmail.com>
2019-02-24 17:00:40 -0700

    Deleted main test func

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdio.h>
-#include "clFramework.h"
-
-int main(int argc, char* argv[]){
-    clInfo *cli = new clInfo;
-    cli->printInfo();
-    cli->loadKernelsFromFile("kernels.cl");
-}
